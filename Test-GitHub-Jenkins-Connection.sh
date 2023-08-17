727477f994d696780a972d0c0700aa99ba849eb4
sso-vineel <111779903+sso-vineel@users.noreply.github.com>
2023-08-17 19:15:35 +0530

    Update Test-GitHub-Jenkins-Connection.sh
    updated code

--- Test-GitHub-Jenkins-Connection.sh
+++ Test-GitHub-Jenkins-Connection.sh
@@ -1,3 +1,5 @@
 #!bin/bash
 
 echo "Hello Vineel, if you are seeing this message. Then your jenkins is connected to GitHub and it is ready to use"
+echo "==============================================================="
+echo "This is triggered automatically"
